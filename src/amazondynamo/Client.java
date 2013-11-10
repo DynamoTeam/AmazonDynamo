edf3513d6bd7966ab55be7a1740ebb687074f751
Laura Vasiliu <laura@laura.(none)>
2013-11-10 15:54:22 +0200

    Added file with the commands to be transmited to the LoadBalancer. Added read from file functionality to the Client

--- src/amazondynamo/Client.java
+++ src/amazondynamo/Client.java
@@ -4,9 +4,13 @@
  */
 package amazondynamo;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,6 +26,9 @@ public class Client {
     private static Socket clientSocket = null;
     private static ObjectOutputStream output = null;
     
+    public static BufferedReader bufferReader;
+    private static final String FILE_PATH = "/src/amazondynamo/commands";
+   
     public static void initConnection() throws IOException
     {
         clientSocket = new Socket(host, port);
@@ -34,32 +41,59 @@ public class Client {
         clientSocket.close();
     }
     
-    public void SendPut(String key, Object obj) throws IOException
+    static void SendPut(String key, Object obj) throws IOException
     {
         Command put = new Command(Command.PUT, key, null, obj);        
         output.writeObject(put);
     }
     
-    public void SendGet(String key) throws IOException
+    static void SendGet(String key) throws IOException
     {
         Command get = new Command(Command.GET, key);        
         output.writeObject(get);
-    }
+    } 
+    
+    static void readLines()
+    {
+        try {
+            
+            String currentLine;             
+            bufferReader = new BufferedReader(new FileReader(System.getProperty("user.dir")+FILE_PATH));
+            
+            while ((currentLine = bufferReader.readLine()) != null)
+            {
+                Thread.sleep(2);
+                String []tokens = currentLine.split(" ");
+                if(tokens[0].compareTo("put") == 0)
+                {
+                    System.out.println("put");
+                    SendPut(tokens[1],  tokens[2]);
+                }
+                else if(tokens[0].compareTo("get") == 0)
+                {
+                    System.out.println("get");
+                    SendGet(tokens[1]);
+                }            
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        
+    }    
     
     public static void main(String[] args)
     {
-        try {            
+        try { 
             
+            readLines();
             initConnection();
-            
             
         } catch (UnknownHostException ex) {
             System.err.println("Don't know about host " + host);
         } catch (IOException ex) {
              System.err.println("Couldn't get I/O for the connection to the host " + host);
-        }
-        
-        
-        
+        }        
     }
 }
