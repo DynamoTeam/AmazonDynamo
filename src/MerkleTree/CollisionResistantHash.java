e4be7250ab46711323c362df2eaa4f6b65ea055c
Laura Vasiliu <laura@laura.(none)>
2014-01-11 11:37:38 +0200

    Added Merkle Tree package Signed-off-by: Laura Vasiliu <laura@laura.(none)>

---
+++ src/MerkleTree/CollisionResistantHash.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MerkleTree;
+
+/**
+ * A collision-resistant hash. 
+ * Given a "properly" created instance of a collision-resistant hash, it
+ * should be computationally hard to find to different values in the
+ * domain that map to the same value in the range. 
+ * @author talm
+ *
+ * @param <C> Range of the hash function
+ * @param <P> Domain of the hash function
+ */
+public interface CollisionResistantHash<C, P> {
+	/**
+	 * Compute the hash of an element.
+	 * @param element
+	 * @return the hash of element.
+	 */
+	public C hash(P element);
+}
---
+++ src/MerkleTree/Pair.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MerkleTree;
+
+/**
+ *
+ * @author laura
+ */
+/**
+ * A type-safe pair of elements.
+ * @author talm
+ *
+ * @param <A> type of first element.
+ * @param <B> type of second element.
+ */
+public class Pair<A, B> {
+	final public A a;
+	final public B b;
+	
+	public Pair(A a, B b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	@SuppressWarnings(value="unchecked")
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Pair))
+			return false;
+		
+		Pair<A,B> other = (Pair<A,B>) obj;
+		
+		return GenericsUtils.deepEquals(a, other.a) && GenericsUtils.deepEquals(b, other.b);
+	}
+}
